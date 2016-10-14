c94fe86ece7920dc4b1c041e4b12d271a8abf421
Michael Vasey <michaeljvasey@gmail.com>
2016-10-14 21:29:13 +0100

    add create cards function

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,16 +1,31 @@
 console.log("JS file is connected to HTML! Woo!");
 
 // define card variables
-var cardOne = "queen",
-	cardTwo = "queen",
-	cardThree = "king",
-	cardFour = "king",
-	msg = '';
+// var cardOne = "queen",
+// 	cardTwo = "queen",
+// 	cardThree = "king",
+// 	cardFour = "king";
+// 	msg = '';
 
-if (cardOne == cardThree) {
-	msg = "You found a match!";
-} else {
-	msg = "Sorry, try again"
-}
+// if (cardOne == cardThree) {
+// 	msg = "You found a match!";
+// } else {
+// 	msg = "Sorry, try again"
+// }
 
-alert(msg);
+// alert(msg);
+
+var board = document.getElementById('game-board');
+
+var i, card;
+
+var createCards = function() {
+	for(i = 0; i < 4; i++) {
+		card = document.createElement('div');
+		card.className = 'card';
+		board.appendChild(card);
+	}
+};
+
+createCards();
+
